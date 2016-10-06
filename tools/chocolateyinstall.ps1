d0ff6b23ef28bc737cbc37971bf74d7f8e5e55c3
jmbeach <jaredbeachdesign@gmail.com>
2016-10-06 14:24:24 -0500

    init

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'grayboxsimulator'
+$url = 'http://gray-box.net/files/gray_simulator.zip'
+$url64 = 'http://gray-box.net/files/gray_simulator.zip'
+$zipPath = $env:temp + "\\gray.zip"
+$unzipPath = $env:temp + "\\gray"
+$unzipFile = $unzipPath + "\\gbopcsim18.msi"
+
+Get-ChocolateyWebFile $packageName $zipPath $url $url64
+Get-ChocolateyUnzip $zipPath $unzipPath
+Install-ChocolateyInstallPackage $packageName "msi" "/quiet" $unzipFile
